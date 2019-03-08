b36af0723fb2253f3ba97dc4b8f114f000020e7a
kangste3 <kangste3@DESKTOP-L29KUVN.localdomain>
2019-03-08 18:21:41 -0500

    lab8

--- lab8/checkpasswd.c
+++ lab8/checkpasswd.c
@@ -16,7 +16,13 @@ int main(void) {
   char user_id[MAXLINE];
   char password[MAXLINE];
   int pipe_fd[2];
-
+  int status; 
+  int result;
+  
+  //Must memeset to all nulls so its length of MAX_PASSWORD
+  memset(user_id, '\0', MAX_PASSWORD);
+  memset(password, '\0', MAX_PASSWORD);
+  
   if(fgets(user_id, MAXLINE, stdin) == NULL) {
       perror("fgets");
       exit(1);
@@ -27,26 +33,56 @@ int main(void) {
   }
   
   
-  if ((pipe(pipe_fd[i-1])) == -1) {
+  if ((pipe(pipe_fd)) == -1) {
 	perror("pipe");
 	exit(1);
   }
-  //Must memeset to all nulls so its length of MAX_PASSWORD
+
+  close(pipe_fd[0]);
   write(pipe_fd[1], user_id, MAXLINE);
   write(pipe_fd[1], password, MAXLINE);
 	
-  dup2(pipe_fd[0], 0);
-
   int ret;
-  pipe(pipe_fd); 
+  char *argv[1];
+  argv[0] = NULL;
+  
+  ret = fork(); 
+  
   if (ret == -1){
 	perror("fork error");
 	exit(1);
   } 
   else if(ret == 0){
+  	  close(pipe_fd[1]); 
+  	  dup2(pipe_fd[0], 0);
+  	  execvp("validate", argv);
+  	  //Child process will exit in validate.c	
+  }
+  else {
+  	wait(&status);
+  	if (WIFEXITED(status)){
+  		result = WEXITSTATUS(status); 
+    }
+    close(pipe_fd[1]); 
+  }
+
+  //Display result
+  if(result == 0){
+  	printf(SUCCESS);
+  	
+  }else if(result == 2){
+  	printf(INVALID);
+  	
+  }else if(result == 3){
+  	printf(NO_USER);
   	
   }
 
-
   return 0;
 }
+
+
+
+
+
+
