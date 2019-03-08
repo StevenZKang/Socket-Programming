bd36c6e5b5b326d92b9cc8ea5aab19910de072f4
Kang Steven Zeren <kangste3@teach.cs.toronto.edu>
2019-03-08 15:05:11 -0500

    lab8 1

--- lab8/checkpasswd.c
+++ lab8/checkpasswd.c
@@ -15,6 +15,7 @@
 int main(void) {
   char user_id[MAXLINE];
   char password[MAXLINE];
+  int pipe_fd[2];
 
   if(fgets(user_id, MAXLINE, stdin) == NULL) {
       perror("fgets");
@@ -25,6 +26,26 @@ int main(void) {
       exit(1);
   }
   
+  
+  if ((pipe(pipe_fd[i-1])) == -1) {
+	perror("pipe");
+	exit(1);
+  }
+  //Must memeset to all nulls so its length of MAX_PASSWORD
+  write(pipe_fd[1], user_id, MAXLINE);
+  write(pipe_fd[1], password, MAXLINE);
+	
+  dup2(pipe_fd[0], 0);
+
+  int ret;
+  pipe(pipe_fd); 
+  if (ret == -1){
+	perror("fork error");
+	exit(1);
+  } 
+  else if(ret == 0){
+  	
+  }
 
 
   return 0;
